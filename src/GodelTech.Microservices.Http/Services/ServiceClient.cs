89cdba198f7123df4f86b6243d1ad77be81adea6
diamond_dragon <diamond_dragon@tut.by>
2020-05-13 22:54:47 +0300

    Minor changes

--- src/GodelTech.Microservices.Http/Services/ServiceClient.cs
+++ src/GodelTech.Microservices.Http/Services/ServiceClient.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using GodelTech.Microservices.Core.Exceptions;
+using CollaborationException = GodelTech.Microservices.Http.Exceptions.CollaborationException;
 
 namespace GodelTech.Microservices.Http.Services
 {
