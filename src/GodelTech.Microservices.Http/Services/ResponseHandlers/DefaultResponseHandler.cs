5474dbe609a61f479a806bdb68994443f423efe8
diamond_dragon <diamond_dragon@tut.by>
2022-09-26 23:55:11 +0200

    Async implementation fix

--- src/GodelTech.Microservices.Http/Services/ResponseHandlers/DefaultResponseHandler.cs
+++ src/GodelTech.Microservices.Http/Services/ResponseHandlers/DefaultResponseHandler.cs
@@ -13,15 +13,20 @@ namespace GodelTech.Microservices.Http.Services.ResponseHandlers
             _jsonSerializer = jsonSerializer ?? throw new ArgumentNullException(nameof(jsonSerializer));
         }
 
-        public async Task<T> ReadContentAsync<T>(HttpResponseMessage response)
+        public Task<T> ReadContentAsync<T>(HttpResponseMessage response)
         {
             if (response == null)
                 throw new ArgumentNullException(nameof(response));
 
+            return ReadContentInternalAsync<T>(response);
+        }
+
+        private async Task<T> ReadContentInternalAsync<T>(HttpResponseMessage response)
+        {
             var stringContent = await response.Content.ReadAsStringAsync();
 
-            return string.IsNullOrWhiteSpace(stringContent) ?
-                default :
+            return string.IsNullOrWhiteSpace(stringContent) ? 
+                default : 
                 _jsonSerializer.Deserialize<T>(stringContent);
         }
     }
